3873a0b9c6b31c9fef447ad40b73b3f4dd2a2bfb
Praful Shah <130747075+psychonimus@users.noreply.github.com>
2024-06-13 16:27:40 +0530

    1st commit 13june

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.querySelectorAll('a[href="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 
 const swiper = new Swiper('.sample-slider', {
     loop: true,
@@ -7,4 +18,33 @@ const swiper = new Swiper('.sample-slider', {
     speed: 3000,          //add
     slidesPerView: 5,     //add
 })
+
+
+var links = document.querySelectorAll('.dropdown-item');
+var btn = document.getElementById('close');
+// var btn2 = document.getElementById('close-2');
+
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        // console.log('clicked');
+        btn.click(); // Click the button
+        // btn2.click(); // Click the button
+
+        // Synchronize the button state with the dropdown state
+        var checkbox = btn.querySelector('input[type="checkbox"]');
+        checkbox.checked = !checkbox.checked; // Toggle the checked state
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
